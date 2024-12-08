5ba0e1c8c44accbe4952549dbd1c7ebb50defe34
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2024-12-08 14:50:26 +0300

    Add s3 service

---
+++ src/Common/S3/Service/IS3Service.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.S3.Service;
+
+public interface IS3Service
+{
+	public Task UploadFileAsync(string keyName, Stream data, string bucketName);
+
+	public Task DeleteFileAsync(string keyName, string bucketName);
+
+	public Task<byte[]> GetFileAsync(string keyName, string bucketName);
+
+	public Task CreateBucketAsync(string bucketName);
+	public Task DeleteBucketAsync(string bucketName);
+}
