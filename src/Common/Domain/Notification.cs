4cfbaa8093075a2df99620bb4a4b0d4810fb5fe3
MaxPlays35 <35427519+MaxPlays35@users.noreply.github.com>
2024-12-14 19:39:30 +0300

    fix: JobId type

--- src/Common/Domain/Notification.cs
+++ src/Common/Domain/Notification.cs
@@ -4,7 +4,7 @@ public class Notification
 {
     public long Id { get; set; }
     public string Text { get; set; } = string.Empty;
-    public int JobId { get; set; }
+    public long JobId { get; set; }
     public DateTime CreatedAt { get; set; }
     
     public Job Job { get; set; }
