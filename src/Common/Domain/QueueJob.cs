f5d0f58b7c23e690fcf4c1c25a6725f869fa1a30
MaxPlays35 <35427519+MaxPlays35@users.noreply.github.com>
2024-12-14 18:15:55 +0300

    fix: domain entity

--- src/Common/Domain/QueueJob.cs
+++ src/Common/Domain/QueueJob.cs
@@ -5,6 +5,7 @@ public class QueueJob
     public long Id { get; set; }
     public long JobId { get; set; }
     public short Progress { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     
     public Job Job { get; set; }
 }
